9104e0c91a491a4688b087511f00854908044dbc
old789 <old789@users.noreply.github.com>
2023-05-23 11:15:22 +0300

    Update emulator_a4988.ino
    more works

--- emulator_a4988.ino
+++ emulator_a4988.ino
@@ -1,12 +1,24 @@
-define LED_BUILTIN 8
+#define INPUT_STEP 2
+#define INPUT_DIRECTION 3
+#define STEPPERa1 4
+#define STEPPERa2 5
+#define STEPPERb1 6
+#define STEPPERb2 7
+#define LED 8
+
+#include <AccelStepper.h>
+
+AccelStepper motor1(4, STEPPERa1, STEPPERa2, STEPPERb1, STEPPERb2);
 
 void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode( INPUT_STEP, INPUT );
+  pinMode( INPUT_DIRECTION, INPUT );
+  pinMode(LED, OUTPUT);
 }
 
 void loop()
 {
-  digitalWrite(LED_BUILTIN, digitalRead(LED_BUILTIN) ^ HIGH);
+  digitalWrite(LED, digitalRead(LED) ^ HIGH);
   delay(2000);
 }
