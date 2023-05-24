2f90d44a7b46999a0401e3e0efc676e04441d49d
old789 <old789@users.noreply.github.com>
2023-05-24 12:37:29 +0300

    Update emulator_a4988.ino
    watchdog

--- emulator_a4988.ino
+++ emulator_a4988.ino
@@ -18,14 +18,29 @@
 #define STEPS_BACK -1 * STEPS_FORWARD
 
 #define   NOP __asm__ __volatile__ ("nop\n\t")
- 
+
+#include <avr/wdt.h>
 #include <Stepper.h>
 
 uint8_t state = LOW;
 unsigned long prev_timestamp = 0;
-uint16_t tick = 0;
+uint16_t tick_led = 0;
+uint16_t tick_wdt = 0;
 
 Stepper motor(STEPS, STEPPERa1, STEPPERa2, STEPPERb1, STEPPERb2);
+
+/* 
+ * без скидання MCUSR буде bootloop після спрацювання watchdog після включення живлення на чіпах PA & PB
+ * що цікаво, після скидання сигналом RESET або у випадку чіпів P, такого ефекту нема.
+ * Взагалі-то досить код із цієї функції помістити в функцію setup, але мануал на чіп радить так.
+ */
+ 
+void clr_mcusr(void) __attribute__((naked)) __attribute__((section(".init3")));
+
+void clr_mcusr(void){
+  MCUSR = 0;
+  wdt_disable();
+}
 
 void setup() {
   pinMode( INPUT_STEP, INPUT );
@@ -33,16 +48,17 @@ void setup() {
   pinMode( LED, OUTPUT );
   pinMode(LED_BUILTIN, OUTPUT);
   motor.setSpeed( RPM );
-#ifdef USE_SERIAL
+# ifdef USE_SERIAL
   Serial.begin(9600);
   delay(2000);
   Serial.println("Booted");
-#endif
+# endif
+  wdt_enable (WDTO_250MS);
 }
 
 void loop() {
   int8_t d = 1;
-  blink();
+  heartbeat();
    if ( digitalRead( INPUT_STEP ) == LOW ){
     if ( state == HIGH ) {
       state = LOW;
@@ -57,10 +73,10 @@ void loop() {
   digitalWrite( LED, HIGH );
   state = HIGH;
   d = digitalRead( INPUT_DIRECTION ) == LOW ? STEPS_BACK : STEPS_FORWARD;
-#ifdef USE_SERIAL
+# ifdef USE_SERIAL
   Serial.print("Step(s):");
   Serial.println(d);
-#endif
+# endif
   motor.step(d);
 }
 
@@ -73,20 +89,30 @@ void nop5() {
   NOP;  
 }
 
-void blink() {
+void heartbeat() {
 unsigned long timestamp = millis();
+uint16_t delta = 0; 
 
   if ( timestamp < prev_timestamp ) {
-    tick += (uint16_t)( 0xffffffff - prev_timestamp + timestamp );
+    delta = (uint16_t)( 0xffffffff - prev_timestamp + timestamp );
   }else{
-    tick += (uint16_t)( timestamp - prev_timestamp );
+    delta = (uint16_t)( timestamp - prev_timestamp );
   }
   prev_timestamp = timestamp;
-  if ( tick > 1000 ) {
-#ifdef USE_SERIAL
-  Serial.println("Tick");
-#endif
-    tick = 0;
+  tick_led += delta;
+  tick_wdt += delta;
+  
+  if ( tick_wdt > 75 ) { 
+    wdt_reset();
+    tick_wdt = 0;
+  }
+  
+  if ( tick_led > 1000 ) {
+#   ifdef USE_SERIAL
+    Serial.println("Tick");
+#   endif
+    tick_led = 0;
     digitalWrite( LED_BUILTIN, digitalRead( LED_BUILTIN ) ^ HIGH );
   }
+  
 }
