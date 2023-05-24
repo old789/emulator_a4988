9355378ce6c8ab33ba12d71ca4dfefd4b67cef96
old789 <old789@users.noreply.github.com>
2023-05-24 11:12:23 +0300

    Update emulator_a4988.ino
    some debugging and impovement

--- emulator_a4988.ino
+++ emulator_a4988.ino
@@ -1,5 +1,6 @@
-// #define USE_SERIAL
+#define USE_SERIAL
 
+// pins 
 #define INPUT_STEP 2
 #define INPUT_DIRECTION 3
 #define STEPPERa1 4
@@ -7,15 +8,20 @@
 #define STEPPERb1 6
 #define STEPPERb2 7
 #define LED 8
+#define LED_BUILTIN 14
 
+// motor
 #define STEPS 100 //  360 / 3.6
 #define RPM 30
+#define STEPS_FORWARD 3
+#define STEPS_BACK -1 * STEPS_FORWARD
 
 #define   NOP __asm__ __volatile__ ("nop\n\t")
  
 #include <Stepper.h>
 
 uint8_t state = LOW;
+uint16_t tick = 0;
 
 Stepper motor(STEPS, STEPPERa1, STEPPERa2, STEPPERb1, STEPPERb2);
 
@@ -23,16 +29,18 @@ void setup() {
   pinMode( INPUT_STEP, INPUT );
   pinMode( INPUT_DIRECTION, INPUT );
   pinMode( LED, OUTPUT );
+  pinMode(LED_BUILTIN, OUTPUT);
   motor.setSpeed( RPM );
 #ifdef USE_SERIAL
   Serial.begin(9600);
+  delay(2000);
   Serial.println("Booted");
 #endif
 }
 
 void loop() {
-  uint8_t d = 1;
-  if ( digitalRead( INPUT_STEP ) == LOW ){
+  int8_t d = 1;
+   if ( digitalRead( INPUT_STEP ) == LOW ){
     if ( state == HIGH ) {
       state = LOW;
       digitalWrite( LED, LOW );
@@ -45,7 +53,11 @@ void loop() {
   }
   digitalWrite( LED, HIGH );
   state = HIGH;
-  d = digitalRead( INPUT_DIRECTION ) == LOW ? -1 : 1;
+  d = digitalRead( INPUT_DIRECTION ) == LOW ? STEPS_BACK : STEPS_FORWARD;
+#ifdef USE_SERIAL
+  Serial.print("Step(s):");
+  Serial.println(d);
+#endif
   motor.step(d);
 }
 
